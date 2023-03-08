8125bde6ecabf86fba3740e69e5a11cc859aa310
Soulzin01 <112558190+Soulzin01@users.noreply.github.com>
2023-03-08 11:14:22 -0300

    commit starting

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        burtons: "burtons",
+        acumin: "acumin",
+        centauri: "centauri",
+      },
+
+      minHeight:{
+        '3/4': '75%',
+      }
+    },
+  },
+  plugins: [],
+}
