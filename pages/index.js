780971486c57b077150357cd5fc0bc8f2e99cee9
Soulzin01 <112558190+Soulzin01@users.noreply.github.com>
2023-03-09 00:59:08 -0300

    commit alter card

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import {BsMoonStarsFill} from 'react-icons/bs';
 import {AiFillLinkedin, AiFillInstagram, AiOutlineWhatsApp} from 'react-icons/ai';
 import Image from 'next/image';
 import devsoul from "../public/joaoImg.png";
+import iconDesign from "../public/iconDesign.png";
 
 export default function Home() {
   return (
@@ -21,10 +22,6 @@ export default function Home() {
               <h1 className='text-xl text-slate-200 font-centauri cursor-default'>CT</h1>
               <ul className='flex items-center'>
                 <li><BsMoonStarsFill className='cursor-pointer text-2xl text-slate-200'/></li>
-                <li>
-                  <a className='bg-gradient-to-tr from-purple-800 to-slate-800 text-white px-4 py-2 rounded-md ml-8 font-burtons'
-                   href="#">Resume</a>
-                </li>
               </ul>
             </nav>
 
@@ -32,27 +29,32 @@ export default function Home() {
               <h2 className='text-5xl text-purple-700 font-medium  py-2'>João Emanuel</h2>
               <h3 className='text-2xl text-slate-100 py-2'>Técnico em Informática | Designer</h3>
               <p className='text-lg text-slate-50 py-4'>
-                Freelancer, trabalho com desenvolvimento de aplicações desktop com a linguagem
-                Java, e tento desenvolver para Web.
+                
               </p>
             </div>
 
             <div className='text-4xl text-center flex text-slate-50 justify-center gap-9'>
+              
                 <a href="https://www.linkedin.com/in/jo%C3%A3o-alcanfor-341363249/" target="_blank" rel="noopener">
                 <AiFillLinkedin/>
                 </a>
+
                 <a href="https://www.instagram.com/conjunto_tecnico/" target="_blank" rel="noopener">
                 <AiFillInstagram/>
                 </a>
+
                 <a href="https://api.whatsapp.com/send/?phone=5588988464056&text&type=phone_number&app_absent=0" target='_blank' rel="noopener">
                 <AiOutlineWhatsApp/>
                 </a>
+
               </div>
 
               <div className="relative rounded-full w-72 h-72 overflow-clip mx-auto mt-10 border-slate-600 border-4">
                 <Image className='object-cover' src={devsoul} fill/>
               </div>
+
           </section>
+
           <section>
             <div className='max-w-2xl mx-auto py-10'>
                 <h3 className="text-3xl text-center text-slate-50">
@@ -63,6 +65,24 @@ export default function Home() {
                 e há 1 ano atuo como <span className='text-teal-300'>designer gráfico de digitais e impressos</span> (logotipos, publicações, embalagens, flyers,
                  documentos entre outros).
                 </p>
+            </div>
+            <div className="">
+              <div className="text-center">
+                <Image src={iconDesign} width={100} height={100}/>
+                <h3 className="text-1xl font-medium pt-8 pb-2 text-slate-50">
+                  Designs Profissionais
+                </h3>
+                <p className="text-lg text-slate-50">
+                  Criação de designs elegantes que atenderão às suas necessidades.
+                </p>
+                <h4 className="text-teal-300 py-4">
+                  Ferramentas que uso:
+                </h4>
+                <p className='text-slate-50 font-medium py-1'>Photoshop</p>
+                <p className='text-slate-50 font-medium py-1'>Adobe Illustrator</p>
+                <p className='text-slate-50 font-medium py-1'>Canva</p>
+                <p className='text-slate-50 font-medium py-1'>Corel Draw</p>
+              </div>
             </div>
           </section>
       </main>
